0651462fd158beb9a5857ae2d31d5a9f952768d0
성수 <whtjdtn312@gmail.com>
2022-07-03 00:01:52 +0900

    Vue-study

---
+++ hello-world/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hello-world/src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="hello">
+    <!-- <h1>{{message}}</h1> -->
+    <h3>이름: {{ firstName }}</h3>
+    <h3>성: {{ lastName }}</h3>
+    <h3>풀네임 : {{ fullName }}</h3>
+    <button @click="fullNameGS = 'zerobase school'" >setter button</button>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HelloWorld',
+  // props: ['todo'],
+  data() {
+    return {
+      message: '안녕하세요! vue world',
+      firstName: 'sungsu',
+      lastName: 'CHO'
+    }
+  },
+  computed: {
+    fullName() {
+      return this.firstName + ' ' + this.lastName
+    },
+    fullNameGS: {
+      get: function () {
+        return this.firstName + ' ' + this.lastName
+      },
+      set: function (e) {
+        const names = e.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[names.length - 1]
+      }
+
+    }
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
